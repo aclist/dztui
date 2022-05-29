1912e0c4b0208a4694202d43141c070d52074e3d
aclist <92275929+aclist@users.noreply.github.com>
2022-05-29 14:47:25 +0900

    Restore title line with working separator

--- dzgui.sh
+++ dzgui.sh
@@ -188,7 +188,7 @@ set_mode(){
 main_menu(){
 	while true; do
 	set_mode
-	sel=$(zenity --width=1280 --height=800 --list --title="DZGUI" --text="DZGUI $version" --cancel-label="Exit" --ok-label="Select" --column="Select launch option" "${items[@]}" 2>/dev/null)
+	sel=$(zenity --width=1280 --height=800 --list --title="DZGUI" --text="DZGUI $version | Mode: $mode | Fav: (not implemented)" --cancel-label="Exit" --ok-label="Select" --column="Select launch option" "${items[@]}" 2>/dev/null)
 	if [[ $? -eq 1 ]]; then
 		exit
 	elif [[ -z $sel ]]; then
