462643e2126e2552050a1adf21795f7f3cc630f8
aclist <92275929+aclist@users.noreply.github.com>
2022-05-29 15:29:39 +0900

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yad_path="/lib/yad"
-yad=$(echo "$(dirname -- ${BASH_SOURCE[0]})${yad_path}")
-${yad} --button="Ok":0 --text="\nTesting yad" --text-align=center --center --on-top --width=300
