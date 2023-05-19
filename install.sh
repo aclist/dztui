b92921e5a2fb69acdd1832ad8baf062dc5287755
aclist <92275929+aclist@users.noreply.github.com>
2023-05-19 23:26:29 +0900

    fix: clear files on reinstall

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 curl "https://raw.githubusercontent.com/aclist/dztui/dzgui/dzgui.sh" > dzgui.sh
 chmod +x dzgui.sh
+xdg_file="$HOME/.local/share/applications/dzgui.desktop"
+share="$HOME/.local/share/dzgui"
+conf="$HOME/.config/dztui"
+[[ -f $xdg_file ]] && rm $xdg_file
+[[ -d $share ]] && rm -rf $share
+[[ -d $conf ]] && rm -rf $conf
 ./dzgui.sh
