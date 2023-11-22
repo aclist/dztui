87ad1acb7d35e80000b8f19a4ac7659663f83f04
aclist <92275929+aclist@users.noreply.github.com>
2023-11-23 00:57:09 +0900

    fix: update installscript

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ curl "https://raw.githubusercontent.com/aclist/dztui/testing/dzgui.sh" > dzgui.s
 chmod +x dzgui.sh
 xdg_file="$HOME/.local/share/applications/dzgui.desktop"
 share="$HOME/.local/share/dzgui"
-conf="$HOME/.config/dztui"
 [[ -f $xdg_file ]] && rm $xdg_file
-[[ -d $share ]] && rm -rf $share
-[[ -d $conf ]] && rm -rf $conf
+[[ -d $share ]] && rm -rf "$share"
 ./dzgui.sh
