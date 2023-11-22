caba452afa3af00fc94eeb7803878ef380d8a068
aclist <92275929+aclist@users.noreply.github.com>
2023-11-22 18:59:46 +0900

    chore: normalize installscript

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-curl "https://raw.githubusercontent.com/aclist/dztui/testing/dzgui.sh" > dzgui.sh
+curl "https://raw.githubusercontent.com/aclist/dztui/main/dzgui.sh" > dzgui.sh
 chmod +x dzgui.sh
 xdg_file="$HOME/.local/share/applications/dzgui.desktop"
 share="$HOME/.local/share/dzgui"
 conf="$HOME/.config/dztui"
 [[ -f $xdg_file ]] && rm $xdg_file
-[[ -d $share ]] && rm -rf $share
-[[ -d $conf ]] && rm -rf $conf
+[[ -d $share ]] && rm -rf "$share"
+[[ -d $conf ]] && rm -rf "$conf"
 ./dzgui.sh
