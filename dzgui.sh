810f4dbdc6ef28da8cf8cf34d69cd1978918a4cc
aclist <92275929+aclist@users.noreply.github.com>
2024-01-13 05:32:39 +0900

    chore: update sums

--- dzgui.sh
+++ dzgui.sh
@@ -526,10 +526,10 @@ fetch_dzq(){
 fetch_helpers_by_sum(){
     declare -A sums
     sums=(
-        ["ui.py"]="8c430f1465995cbd98f4ad8e2580a79d"
+        ["ui.py"]="6870f75ff74a8ad65d82650499d739de"
         ["query_v2.py"]="1822bd1769ce7d7cb0d686a60f9fa197"
         ["vdf2json.py"]="2f49f6f5d3af919bebaab2e9c220f397"
-        ["funcs"]="d2537e9b7916453fbfe55592d92e683f"
+        ["funcs"]="d9b0e6fa68314c18ac7aad565645948f"
     )
     local author="aclist"
     local repo="dztui"
