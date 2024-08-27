60b6648218c5ad0cb188eadb8eb0a31cca81cb9a
aclist <92275929+aclist@users.noreply.github.com>
2024-08-28 07:13:09 +0900

    fix: uncomment function call

--- dzgui.sh
+++ dzgui.sh
@@ -864,7 +864,7 @@ initial_setup(){
     watcher_deps
     check_architecture
     test_connection
-#    fetch_helpers > >(pdialog "Checking helper files")
+    fetch_helpers > >(pdialog "Checking helper files")
     varcheck
     source "$config_file"
     lock
