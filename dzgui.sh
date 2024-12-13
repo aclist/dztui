3fce2cc3ad710781029453da458d60ae6d45b9fe
aclist <92275929+aclist@users.noreply.github.com>
2024-12-13 10:18:56 +0900

    fix: return codes

--- dzgui.sh
+++ dzgui.sh
@@ -581,7 +581,7 @@ fetch_helpers_by_sum(){
         ["ui.py"]="be3da1e542d14105f4358dd38901e25a"
         ["query_v2.py"]="55d339ba02512ac69de288eb3be41067"
         ["vdf2json.py"]="2f49f6f5d3af919bebaab2e9c220f397"
-        ["funcs"]="2a27ab36d5f8071fe53bf8dfd3b8d88d"
+        ["funcs"]="10c7d9cb9fbb792626ec9e7a4a788ba5"
         ["lan"]="c62e84ddd1457b71a85ad21da662b9af"
     )
     local author="aclist"
