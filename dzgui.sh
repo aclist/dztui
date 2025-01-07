c8f7a32284a87226c7da8ae506c7ec7cd4d98653
aclist <92275929+aclist@users.noreply.github.com>
2025-01-07 21:39:16 +0900

    chore: update db checksum

--- dzgui.sh
+++ dzgui.sh
@@ -636,7 +636,7 @@ fetch_helpers_by_sum(){
 }
 fetch_geo_file(){
     # for binary releases
-    local geo_sum="7b6668eb4535bb432acb42016ba9cc47"
+    local geo_sum="28ccd75b3e03cf07a7011f22ef0cd69b"
     local km_sum="b038fdb8f655798207bd28de3a004706"
     local gzip="$helpers_path/ips.csv.gz"
     if [[ ! -f $geo_file  ]] || [[ $(get_hash $geo_file) != $geo_sum ]]; then
