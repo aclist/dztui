aclist/dztui

commit 13c6813c8e0d9fe9a1ab8cdb3eee7c78b4bf7919

feat: resolve ip when saving records

helpers/ui.py
@@ -123,6 +123,11 @@ class RowType(EnumWithAttrs):
             "label": None,
             "tooltip": None,
             }
+    RESOLVE_IP = {
+            "label": "Resolve IP",
+            "tooltip": None,
+            "wait_msg": "Resolving remote IP"
+            }
     HIGHLIGHT = {
             "label": "Highlight stale",
             "tooltip": None,
@@ -696,6 +701,11 @@ def process_tree_option(input, treeview):
             msg = out[-1]
             process_shell_return_code(transient_parent, msg, rc, input)
 
+    if command == RowType.RESOLVE_IP:
+        record = "%s:%s" %(treeview.get_column_at_index(7), treeview.get_column_at_index(8))
+        wait_msg = command.dict["wait_msg"]
+        call_on_thread(True, cmd_string, wait_msg, record)
+        return
     # help pages
     if context == WindowContext.TABLE_MODS and command == RowType.HIGHLIGHT:
         wait_msg = command.dict["wait_msg"]
@@ -1095,12 +1105,10 @@ class TreeView(Gtk.TreeView):
         match context_menu_label:
             case "Add to my servers" | "Remove from my servers":
                 record = "%s:%s" %(self.get_column_at_index(7), self.get_column_at_index(8))
-                proc = call_out(parent, context_menu_label, record)
+                process_tree_option([self.view, RowType.RESOLVE_IP], self)
                 if context == "Name (My saved servers)":
                     iter = self.get_current_iter()
                     server_store.remove(iter)
-                msg = proc.stdout
-                res = spawn_dialog(parent, msg, Popup.NOTIFY)
             case "Remove from history":
                 record = "%s:%s" %(self.get_column_at_index(7), self.get_column_at_index(8))
                 call_out(parent, context_menu_label, record)

commit be8bf7440168bda027333a5f196452ef0fb0a679

fix: resolve regression introduced with 13c6813

dzgui.sh
@@ -587,10 +587,10 @@ fetch_helpers_by_sum(){
     [[ -f "$config_file" ]] && source "$config_file"
     declare -A sums
     sums=(
-        ["ui.py"]="d3ad9153d8599bea0eede9fd3121ee8e"
+        ["ui.py"]="5a876efacf208d12b5fe761996425412"
         ["query_v2.py"]="55d339ba02512ac69de288eb3be41067"
         ["vdf2json.py"]="2f49f6f5d3af919bebaab2e9c220f397"
-        ["funcs"]="6a1c7ce585d9b76e2e75dba9d4295f8d"
+        ["funcs"]="417bd5eaffbefc905a843985c691dc64"
         ["lan"]="c62e84ddd1457b71a85ad21da662b9af"
     )
     local author="aclist"

helpers/funcs
@@ -130,6 +130,7 @@ declare -A funcs=(
 ["Remove from my servers"]="update_favs_from_table"
 ["Remove from history"]="remove_from_history"
 ["Force update local mods"]="force_update"
+["Resolve IP"]="resolve_ip"
 ["Handshake"]="final_handshake"
 ["get_player_count"]="get_player_count"
 ["lan_scan"]="lan_scan"
@@ -160,6 +161,33 @@ find_stale_mods(){
     done
     printf ""
     return 99
+}
+resolve_ip(){
+    shift
+    local res
+    local record="$1"
+    local ip=$(<<< "$record" awk -F: '{print $1}')
+    local qport=$(<<< "$record" awk -F: '{print $3}')
+    res=$(a2s $ip $qport info)
+    if [[ ! $? -eq 0 ]]; then
+        printf "Server timed out \n"
+        return 1
+    fi
+    local gport=$(<<< "$res" jq -r '.[].gameport')
+    if [[ -z $gport ]]; then
+        printf "Failed to resolve server metadata\n"
+        return 1
+    fi
+
+    # incoming input can only be 'remove' or 'add'
+    # record is in favs => implies deletion
+    # record not in favs => implies addition
+    resolved_record="$ip:$gport:$qport"
+    if [[ ${ip_list[*]} =~ $resolved_record ]]; then
+        remove_from_favs "$resolved_record"
+    else
+        add_to_favs "$resolved_record"
+    fi
 }
 get_player_count(){
     shift

helpers/ui.py
@@ -1814,10 +1814,16 @@ class TreeView(Gtk.TreeView):
 
 
 def format_metadata(row_sel):
+    # this function is recycled for the add by ip/id methods +
+    # the right-click context menu (add/remove servers)
+    # in the latter case, there is no metadata to update
+    # see grid.update_statusbar(), so the returned row is None
+    row = None
     for i in RowType:
         if i.dict["label"] == row_sel:
             row = i
             prefix = i.dict["tooltip"]
+            break
     vals = {
             "branch": config_vals[0],
             "debug": config_vals[1],
@@ -1827,6 +1833,8 @@ def format_metadata(row_sel):
             "preferred_client": config_vals[5],
             "fullscreen": config_vals[6]
             }
+    if row is None:
+        return None
     try:
         alt = row.dict["alt"]
         default = row.dict["default"]
@@ -2396,6 +2404,8 @@ class Grid(Gtk.Grid):
         return True
 
     def update_statusbar(self, string):
+        if string is None:
+            return
         meta = self.bar.get_context_id("Statusbar")
         self.bar.push(meta, string)
 