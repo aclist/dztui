60ce184d8f4e6a3c41bc4b8115615e8a5339dafa
aclist <92275929+aclist@users.noreply.github.com>
2025-09-11 17:10:16 +0900

    fix: whitespace

--- dzgui.sh
+++ dzgui.sh
@@ -1030,7 +1030,7 @@ initial_setup(){
     watcher_deps
     check_architecture
     test_connection
-   fetch_helpers > >(pdialog "Checking helper files")
+    fetch_helpers > >(pdialog "Checking helper files")
     varcheck
     source "$config_file"
     lock
