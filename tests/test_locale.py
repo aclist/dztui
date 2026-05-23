e715e6da1cc4c2a85a96abf9edd76dee8650e0f2
aclist <92275929+aclist@users.noreply.github.com>
2026-05-23 22:23:44 +0900

    fix: use LC_ALL var in test

--- tests/test_locale.py
+++ tests/test_locale.py
@@ -2,14 +2,14 @@ from dzgui.util import localize
 
 
 def test_de(monkeypatch):
-    monkeypatch.setenv("LC_CTYPE", "de_DE.UTF-8")
+    monkeypatch.setenv("LC_ALL", "de_DE.UTF-8")
     localize.set_locale()
     assert localize.number(9900) == "9.900"
     assert localize.number(99.88) == "99,88"
 
 
 def test_en(monkeypatch):
-    monkeypatch.setenv("LC_CTYPE", "en_US.UTF-8")
+    monkeypatch.setenv("LC_ALL", "en_US.UTF-8")
     localize.set_locale()
     assert localize.number(9900) == "9,900"
     assert localize.number(99.88) == "99.88"
@@ -22,7 +22,7 @@ def test_none(monkeypatch):
 
 
 def test_empty(monkeypatch):
-    monkeypatch.setenv("LC_CTYPE", "")
+    monkeypatch.setenv("LC_ALL", "")
     localize.set_locale()
     assert localize.number(9900) == "9,900"
     assert localize.number(99.88) == "99.88"
