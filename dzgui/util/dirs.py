207d8fd4079d5a7659eb59f1640bbf204022502f
aclist <92275929+aclist@users.noreply.github.com>
2026-07-11 17:52:39 +0900

    chore: add logging

--- dzgui/util/dirs.py
+++ dzgui/util/dirs.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import shutil
 
@@ -6,8 +7,12 @@ from pathlib import Path
 
 from dzgui.const.constants import APP_NAME_LOWER, IMAGES_PATH
 
+logger = logging.getLogger(APP_NAME)
+
+
 def make_parents(path: "Path") -> None:
     path.parent.mkdir(parents=True, exist_ok=True)
+
 
 def copy_dzgui_to_xdg_data(exe_path: Path) -> None:
     dzgui = os.getenv("PYAPP")
@@ -18,6 +23,7 @@ def copy_dzgui_to_xdg_data(exe_path: Path) -> None:
         shutil.copy(dzgui, exe_path)
     except Exception as e:
         logger.critical(e)
+
 
 def find_icon_resource() -> "Path":
     traversable = resources.files(APP_NAME_LOWER).joinpath(IMAGES_PATH)
