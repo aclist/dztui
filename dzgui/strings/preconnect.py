7102c21f5ab04f5c0dd0d6b96d26cb2c8b1b0e38
aclist <92275929+aclist@users.noreply.github.com>
2026-08-14 02:51:12 +0900

    chore: move strings into dedicated file

--- dzgui/strings/preconnect.py
+++ dzgui/strings/preconnect.py
@@ -1,9 +1,7 @@
 update_mods = "Update mods and connect"
 connect = "Connect"
 connect_last = "Load to menu screen"
-connect_last_tooltip = (
-    "Launch DayZ with these mods (if any) and\ngo to the menu screen without connecting."
-)
+connect_last_tooltip = "Launch DayZ with these mods (if any) and\ngo to the menu screen without connecting."
 back = "Back"
 cancel = "Cancel"
 warnings = "Warnings"
@@ -14,3 +12,38 @@ up_to_date = "All mods are up to date."
 all_updated = "All mods updated."
 "If you recently installed {build} or moved it to a different drive, "
 "restart Steam to allow these changes to synchronize, then try again."
+required_space = " Need to download {0} MiB of mod updates."
+
+placeholder_no_mods = "This server has no mods."
+placeholder_no_warnings = "No warnings."
+
+
+# Errors
+invalid_mods = (
+    "Server has invalid mods that are not recognized by Steam.\n"
+    "Contact the server owner and include these mod IDs in your report:\n"
+    "{0}"
+)
+not_enough_space = (
+    "Need to update {0} MiB of mods, but installation path only has {1} MiB."
+)
+resync = (
+    "If you recently installed {0} or moved it to a different drive,\n"
+    "restart Steam to allow these changes to synchronize, then try again."
+)
+steam_not_running = "'{0}' is set as the default Steam client, but it is either not installed or not running."
+use_desktop_mode = "Use Desktop Mode to download mods on Steam Deck."
+version_mismatch = "Local client version '{0}' does not match remote version '{1}'."
+version_missing = "Remote server is running the build '{0}', but it is not installed."
+
+
+# Warnings
+protected_server = (
+    "Protected: you will be prompted for a password when connecting to this server."
+)
+dayz_running = "It looks like DayZ is already running in the background. Exit DayZ before connecting."
+running_app = (
+    "The app '{0}' is currently running in Steam, but background downloads are not enabled.\n"
+    "Either stop the game first, or update your global Steam settings or the game's local settings.\n"
+    "Otherwise, mods may be queued for download but never update."
+)
