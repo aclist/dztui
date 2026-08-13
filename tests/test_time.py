1753a40b942f6b35eefd19839e75a3035298ffd6
aclist <92275929+aclist@users.noreply.github.com>
2026-08-14 08:37:13 +0900

    chore: drop unused test mark

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,10 +1,7 @@
-import pytest
 import re
 
 day = r"([0][7-9]|[1][0-6])"
 night = r"([0][0-6]|[1][7-9]|[2][0-3])"
-
-pytestmark = pytest.mark.FOO
 
 def iterate(h: str, r: str) -> None:
     for m in range(60):
