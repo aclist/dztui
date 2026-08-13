0eb47a04274bce418e65772ab33f2029095af5ce
aclist <92275929+aclist@users.noreply.github.com>
2026-08-14 05:10:41 +0900

    fix: do not start queue checker on phantom right click signals

--- dzgui/views/trees/tree_servers.py
+++ dzgui/views/trees/tree_servers.py
@@ -295,6 +295,11 @@ class ServerTreeView(ContextMixin, TreeView):  # type: ignore
     ) -> None:
         if self.loaded is False:
             return
+        # NOTE: signal triggers twice on right-click events
+        # due to deselect-then-select behavior in GTK
+        model, sel = self.get_selection().get_selected_rows()
+        if len(sel) == 0:
+            return
         self.start_distcalc()
 
     def get_name(self) -> str:
