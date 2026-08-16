cc485289e3f0d7602c7c6fe46db03bc67f286d82
aclist <92275929+aclist@users.noreply.github.com>
2026-08-16 12:46:16 +0900

    feat: abstract field creation into SubmitField

--- dzgui/strings/options.py
+++ dzgui/strings/options.py
@@ -1,12 +1,19 @@
 developers = "Developers"
 start_tab = "Start tab"
 workshop_label = "Subscribed mods"
-workshop_eventbox = "If you manually subscribed to mods on Steam prior to using DZGUI, you can unsubscribe via this link."
 steam_combo = "Steam"
 flatpak_combo = "Flatpak"
 flatpak_container_combo = "Flatpak (container)"
+
+steam_placeholder = "Enter your Steam API key"
+name_placeholder = "Identifies you to other players"
 
 server_combo = "Server browser"
 saved_combo = "Saved servers"
 recent_combo = "Recent"
 lan_combo = "LAN"
+
+fullscreen_eventbox = "This option is not available on Steam Deck."
+save_button = "Save"
+
+api_failed = "API validation failed"
