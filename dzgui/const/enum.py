027e6c0781aaec4259cd30ff11a1698ec6b24173
aclist <92275929+aclist@users.noreply.github.com>
2026-08-19 16:02:59 +0900

    chore: drop unused enums

--- dzgui/const/enum.py
+++ dzgui/const/enum.py
@@ -73,9 +73,6 @@ class Preferences(EnumWithAttrs):
     NAME = {
         "key": "name",
     }
-    INSTALL = {
-        "key": "auto_install",
-    }
     DEFAULT = {
         "key": "default_steam_path",
     }
@@ -87,9 +84,6 @@ class Preferences(EnumWithAttrs):
     }
     IP_LIST = {
         "key": "ip_list",
-    }
-    BRANCH = {
-        "key": "branch",
     }
     START_TAB = {
         "key": "start_tab",
