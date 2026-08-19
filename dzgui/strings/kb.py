5b83a26f6a7959bb27137b1e56bbef35e8b9c7aa
aclist <92275929+aclist@users.noreply.github.com>
2026-08-19 17:44:55 +0900

    chore: reword remarks on MTU

--- dzgui/strings/kb.py
+++ dzgui/strings/kb.py
@@ -10,6 +10,4 @@ By contrast, wireless routers and enterprise-grade routers may be less likely to
 
 If you find that a specific server is unresponsive for you when it shouldn't be, add a port forwarding rule to your router's settings for the server's query port.
 
-In addition, packets received from server responses are expected to be a standard size: MTU (maximum transmission unit) of 1500.
-
-Deviation from this may cause your router to discard incoming responses from the server."""
+In addition, a uniform packet size is expected. If your router's MTU (maximum transmission unit) setting differs from the default of 1500, it could lead to incoming responses being discarded."""
